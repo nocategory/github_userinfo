8a0bfb15fc776587e1ac9ebeb924b15e22e51b4e
Unknown <joaozinn98@gmail.com>
2018-02-14 00:32:19 +0000

    Show a page with N/A if the record isn't populated yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,8 @@ class UsersController < ApplicationController
     def show
         # params[:id] is actually the login nickname
         @user = User.find_by_login(params[:id]) or render file: "#{Rails.root}/public/404", status: 404
+        if !@user.public_repos
+            render file: "#{Rails.root}/public/not_available", status: 200
+        end
     end
 end
